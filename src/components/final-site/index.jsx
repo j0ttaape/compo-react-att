2f5cf6ba2a3cfdc79b35cc08e2478bed41e62e34
j0ttaape <joaopedrotrajano59@gmail.com>
2025-10-09 15:58:43 -0300

    estilizando o componete

---
+++ src/components/final-site/index.jsx
@@ -0,0 +1,34 @@
+import './index.scss'
+
+
+export default function FinalSite(){
+
+    return(
+
+        <section className='sessao-final-site'>
+
+
+        <div className='sessaofinal'>
+
+            <div className='contato'>
+                <p>Nosso contato</p>
+                <p>11 94236-9925</p>
+                <p>livraria2025@gmail.com</p>
+            </div>
+
+            <div className='desenvolvido'>
+
+                <p>Desenvolvido por:</p>
+                <p>João Pedro</p>
+            </div>
+
+            <div className='turma'>
+                <p>Turma:</p>
+                <p>B</p>
+            </div>
+        </div>
+
+
+        </section>
+    )
+}
--- src/pages/inicio/index.jsx
+++ src/pages/inicio/index.jsx
@@ -1,18 +1,31 @@
+import FinalSite from '../../components/final-site'
 import './index.scss'
 import { Link } from 'react-router-dom'
 
-export default function Inicio(){
+export default function Inicio() {
 
-    return(
+    return (
 
         <section className='sessao-inicio'>
 
             <div className='top-site'>
 
-                <div className='objects'>
-                    <Link to={'/sobre'}>Sobre</Link>
+
+                <div className='container-objects'>
+                    <div className='objects'>
+                        <h1>Livraria</h1>
+                        <h1><Link to={'/sobre'}>Sobre</Link></h1>
+                    </div>
                 </div>
             </div>
+
+            <div className='texto-livraria'>
+
+                <h3>Seja bem-vindo à nossa livraria! Aqui, a paixão pela leitura ganha vida. Nossa missão é espalhar o amor pelos livros e criar um espaço acolhedor para todos os amantes da literatura. Conheça nossa equipe dedicada e fique por dentro dos eventos emocionantes que promovemos. Venha explorar um mundo de histórias e conhecimento!</h3>
+
+            </div>
+
+            <FinalSite/>
         </section>
     )
 }
